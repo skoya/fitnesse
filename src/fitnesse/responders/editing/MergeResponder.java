5a6b1d79e3a50ff9cd3f10d9beeacab027e766ae
AlexNetcare <135694182+AlexNetcare@users.noreply.github.com>
2025-12-22 20:17:45 +0100

    Avoid tag deletion on merge (#1590)

--- src/fitnesse/responders/editing/MergeResponder.java
+++ src/fitnesse/responders/editing/MergeResponder.java
@@ -3,7 +3,11 @@
 package fitnesse.responders.editing;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Stream;
 
 import fitnesse.FitNesseContext;
 import fitnesse.Responder;
@@ -17,12 +21,14 @@ import fitnesse.wiki.PageData;
 import fitnesse.wiki.PathParser;
 import fitnesse.wiki.WikiPage;
 import fitnesse.wiki.WikiPagePath;
+import fitnesse.wiki.WikiPageProperty;
 
 public class MergeResponder implements Responder {
   private Request request;
   private String newContent;
   private String existingContent;
   private String resource;
+  private Set<String> suites = new HashSet<>();;
 
   public MergeResponder(Request request) {
     this.request = request;
@@ -36,6 +42,14 @@ public class MergeResponder implements Responder {
     WikiPage page = context.getRootPage().getPageCrawler().getPage(path);
     existingContent = page.getData().getContent();
     newContent = this.request.getInput(EditResponder.CONTENT_INPUT_NAME);
+    // Combining the tags from the first edit with the second one
+    String oldSuites = Optional
+        .ofNullable(page.getData().getProperties().get(WikiPageProperty.SUITES))
+        .orElse("");
+    String newSuites = Optional
+        .ofNullable(this.request.getInput(EditResponder.SUITES)).orElse("");
+    Stream.of(oldSuites.split(",")).forEach(suites::add);
+    Stream.of(newSuites.split(",")).forEach(suites::add);
 
     response.setContent(makePageHtml(context));
 
@@ -51,6 +65,7 @@ public class MergeResponder implements Responder {
     page.put("ticketId", SaveRecorder.newTicket());
     page.put("oldContent", HtmlUtil.escapeHTML(existingContent));
     page.put("newContent", newContent);
+    page.put(EditResponder.SUITES, suites);
     addHiddenAttributes(page);
     return page.html(request);
   }
